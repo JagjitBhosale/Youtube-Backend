099feb10568e8f3e4d53e3d2872b57a452fcd0d8
Jagjit Bhosale <bhosalejagjit@gmail.com>
2025-06-20 17:04:52 +0530

    Added Api error and responses classes in utils , Custom API responses and Error Handling,

--- src/index.js
+++ src/index.js
@@ -15,8 +15,17 @@ dotenv.config({
 })
 
 
-
-connectDB();
+import app from "./app.js"
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at port :${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("MongoDB connection failed", err);
+    
+})
 
 
 
