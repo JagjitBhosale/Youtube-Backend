da36bbb8d87aa286753cafda5defc5df70bb0d7e
Jagjit Bhosale <bhosalejagjit@gmail.com>
2025-06-30 19:52:42 +0530

    Added Middleware and Contollers

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import cookieParser from 'cookie-parser';
 
 const app = express();
+
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials : true,
@@ -14,5 +15,19 @@ app.use(express.urlencoded({extended: true ,limit:"16kb"}))
 app.use(express.static("public"));
 
 app.use(cookieParser());
+
+
+//routes import
+import userRouter from "./routes/user.routes.js"
+
+//routes declaration
+// app.use("/users", userRouter )
+
+// standard practice
+app.use("/api/v1/users", userRouter)
+
+
+// http://localhost:8000/users/regsister
+// http://localhost:8000/api/v1/users/regsister
 
 export default app
